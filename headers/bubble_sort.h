cdafb1dc19e2b08cd7bb2db4ff51c467074e31dd
Michal-Pilarski <michalpilarski17@gmail.com>
2024-10-07 15:06:27 +0200

    Bubble sort implementation

---
+++ headers/bubble_sort.h
@@ -0,0 +1,24 @@
+#ifndef BUBBLE_SORT_H
+#define BUBBLE_SORT_H
+
+#include <vector>
+
+// template
+template <typename T>
+void bubbleSort(std::vector<T>& arr) {
+    bool swapped;
+    int n = arr.size();
+    do {
+        swapped = false;
+        for (int i = 1; i < n; ++i) {
+            if (arr[i - 1] > arr[i]) {
+                std::swap(arr[i - 1], arr[i]);
+                swapped = true;
+            }
+        }
+        // Po każdej iteracji ostatni element jest na właściwej pozycji
+        --n;
+    } while (swapped);
+}
+
+#endif 
